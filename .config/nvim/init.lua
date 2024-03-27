02fc44307f9038cd1f6ac516f56079f5b31a13e1
ttiimmothy <dinosauli2006@gmail.com>
2024-03-27 00:56:23 -0400

    feat: correct nvim init config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,33 +20,41 @@ require('lazy').setup({
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then change the command in the config to whatever the name of that colorscheme is.
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    -- 'catppuccin/nvim',
+    -- 'rebelot/kanagawa.nvim',
+    'kepano/flexoki-neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'catppuccin-macchiato'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.colorscheme 'flexoki-dark'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
+-- {
+--   ui = {
+--     -- If you are using a Nerd Font: set icons to an empty table which will use the default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+--     icons = vim.g.have_nerd_font and {} or {
+--       cmd = '⌘',
+--       config = '🛠',
+--       event = '📅',
+--       ft = '📂',
+--       init = '⚙',
+--       keys = '🗝',
+--       plugin = '🔌',
+--       runtime = '💻',
+--       require = '🌙',
+--       source = '📄',
+--       start = '🚀',
+--       task = '📌',
+--       lazy = '💤 ',
+--     },
+--   },
+-- }
+)
