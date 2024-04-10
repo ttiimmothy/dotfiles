bc055d44b95b1b9894d3b79f0002ac23d70f3a96
ttiimmothy <dinosauli2006@gmail.com>
2024-04-10 12:28:56 -0400

    feat add alias in fish config

--- .config/fish/config-osx-common.fish
+++ .config/fish/config-osx-common.fish
@@ -1,1 +1,3 @@
 alias python "python3"
+alias icond "neofetch --ascii_colors 9 7 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
+alias icon "neofetch --ascii_distro debian --config none -L"
