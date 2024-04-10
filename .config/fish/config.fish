c79a70729a45582f2977c086cfea57132ead409f
ttiimmothy <dinosauli2006@gmail.com>
2024-04-10 04:54:28 -0400

    feat add alias in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   source (dirname (status --current-filename))/config-osx-eza.fish
   source (dirname (status --current-filename))/config-osx-git-command-alias.fish
+  source (dirname (status --current-filename))/config-osx-common.fish
   # source $HOME/.config/fish/config-osx.fish
   set -g fish_greeting
 end
