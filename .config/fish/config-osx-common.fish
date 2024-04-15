e23de88a82c391af07411c698df9f4bc4488c7cd
ttiimmothy <dinosauli2006@gmail.com>
2024-04-15 16:54:56 -0400

    feat add common alias and alias descriptions in fish config

--- .config/fish/config-osx-common.fish
+++ .config/fish/config-osx-common.fish
@@ -1,10 +1,18 @@
+# python
 alias python "python3"
+
+# neofetch
 alias icond "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
 alias icondcolor "neofetch --ascii_colors 7 9 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
 alias icon "neofetch --ascii_colors 9 8 --ascii_distro debian --config none -L"
-alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
+alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none"
+alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
 alias raspberry "neofetch --ascii_colors 8 2 --ascii_distro raspbian --config none"
+
+# other (tmux, neovim, zoxide)
 alias tsession "tmux ls"
 alias vim "nvim"
 alias v "z"
+
+# source
 alias so-z "source ~/.zshrc"
