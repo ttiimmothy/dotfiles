086648cab6aa1bebb1d233471b42a83e7badc8aa
ttiimmothy <dinosauli2006@gmail.com>
2024-04-15 17:02:00 -0400

    feat update common alias in fish config

--- .config/fish/config-osx-common.fish
+++ .config/fish/config-osx-common.fish
@@ -2,10 +2,10 @@
 alias python "python3"
 
 # neofetch
-alias icond "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
-alias icondcolor "neofetch --ascii_colors 7 9 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
+alias icondistro "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
+alias icondistrocolor "neofetch --ascii_colors 7 9 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
 alias icon "neofetch --ascii_colors 9 8 --ascii_distro debian --config none -L"
-alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none"
+alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none --colors 2 7 7 3 7 7"
 alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
 alias raspberry "neofetch --ascii_colors 8 2 --ascii_distro raspbian --config none"
 
