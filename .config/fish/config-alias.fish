26ee0fe44b506f1a3592f165f366aa3cfead5f05
ttiimmothy <dinosauli2006@gmail.com>
2024-04-29 09:45:24 -0400

    feat update fish config file name, doc update file name in readme

---
+++ .config/fish/config-alias.fish
@@ -0,0 +1,39 @@
+# git
+# normal commit to any branch without anything added, but can also combine as git add . && norcomt "message" with things added
+function norcomt
+  # $argv[1] coverts to string if I don't put string as param
+  git co -m "$argv[1]" && git push
+end
+# initial commit
+alias init "git add . && git co -m 'chore init' && git push -u origin main"
+# usually use after emt
+alias emback "git cout main && git pull-allow origin main"
+# checkout empty branch and create empty commit
+alias emt "git cout empty && git pull origin empty && git pull origin main && git co -m "empty" && git push"
+alias git-global "git config --global --list"
+alias glk "git lfs track"
+alias vgit1 "emback && v g vim && norcomt empty && v ../doe && norcomt empty && v d 0 && norcomt empty && v d 1 && norcomt empty && v gi zsh && norcomt empty && v loc fish && norcomt "feat add fish shell history" && v githu shell && emback && v raycast && norcomt empty && v repo repos && norcomt empty && v proj m && emt"
+alias vgit2 "emback && v vsc && norcomt empty && v general && norcomt empty && v gi pseudo && norcomt empty && v codeforce && norcomt empty && v use && norcomt empty && v install && norcomt empty && v metrics && emt"
+
+# python
+alias python "python3"
+alias pip "pip3"
+
+# neofetch
+alias icondistro "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
+alias icondistrocolor "neofetch --ascii_colors 7 9 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
+alias icon "neofetch --ascii_colors 9 8 --ascii_distro debian --config none -L"
+alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none --colors 2 7 7 3 7 7"
+alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
+alias raspberry "neofetch --ascii_colors 8 2 --ascii_distro raspbian --config none"
+
+# other (tmux, neovim, zoxide)
+alias tsession "tmux ls"
+alias vim "nvim"
+alias v "z"
+
+# source
+alias soz "source ~/.zshrc"
+
+# open file
+alias pdfvore "open ~/Integration/voreprank/Perfect\ pdf.pdf"
