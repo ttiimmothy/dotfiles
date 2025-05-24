54c473c6d23c37efa21f23defec7149bf656f4ef
ttiimmothy <dinosauli2006@gmail.com>
2025-05-24 10:14:48 -0400

    feat update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -391,6 +391,9 @@ require("lazy").setup({
         },
         lua_ls = {
           -- capabilities = {},
+          name = 'lua-language-server',
+          cmd = { 'lua-language-server' },
+          root_dir = vim.fs.dirname(vim.fs.find({'.git', '.vim', 'nvim'}, { upward = true })[1]),
           settings = {
             Lua = {
               completion = {
