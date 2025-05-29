3a08c92f869fd56c6369e68c5f9f15a0cf66e890
ttiimmothy <dinosauli2006@gmail.com>
2025-05-29 11:45:41 -0400

    feat remove unused comments in lspconfiguration.lua

--- .config/nvim/lua/various/plugins/lspconfiguration.lua
+++ .config/nvim/lua/various/plugins/lspconfiguration.lua
@@ -50,12 +50,8 @@ return {
           end
         end,
       })
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      -- By default, Neovim doesn't support everything that is in the LSP specification.
-      -- When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      -- We create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
       -- tbl = table
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
       -- For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
