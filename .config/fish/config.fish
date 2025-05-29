edb034b1226eb951d122ea04eafef363a95e4f19
ttiimmothy <dinosauli2006@gmail.com>
2025-05-28 22:40:45 -0400

    feat update zsh configuration, add warp theme color

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,14 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
 if status is-interactive
   # Commands to run in interactive sessions can go here
   source (dirname (status --current-filename))/config-osx-eza.fish
   source (dirname (status --current-filename))/config-alias.fish
-  # source $HOME/.config/fish/config-osx.fish
+  # source $HOME/.config/fish/config-osx-eza.fish
+  # source $HOME/.config/fish/config-alias.fish
+  # echo -e '\n# Auto-Warpify\nstatus --is-interactive; and printf '\''\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish", "uname": "'$(uname)'" }}\x9c'\'' '
+  # Auto-Warpify
+  printf 'P$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish", "uname": "Darwin" }}�'
+  # printf '\''\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish", "uname": "Darwin" }}\x9c'\'' '
   set -g fish_greeting
 end
 zoxide init fish | source
