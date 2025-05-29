2f67275b87ad549c29680b726e4bc7a28cc25645
ttiimmothy <dinosauli2006@gmail.com>
2025-05-29 10:35:18 -0400

    feat remove unused comments in lspconfiguration.lua

--- .config/nvim/lua/various/plugins/lspconfiguration.lua
+++ .config/nvim/lua/various/plugins/lspconfiguration.lua
@@ -57,7 +57,6 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       -- tbl = table
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-      -- Enable the following language servers
       -- For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
