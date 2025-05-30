3b7874ebee1ed45227d45ccc7c217230dff5ef70
ttiimmothy <dinosauli2006@gmail.com>
2025-05-29 23:36:29 -0400

    feat remove unused comments in neovim configuration init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -272,13 +272,7 @@ require("lazy").setup({
     config = function()
       -- Better Around/Inside textobjects
       require("mini.ai").setup({ n_lines = 500 })
-      -- (Add)/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
       require("mini.surround").setup()
-      -- Simple and easy statusline.
       -- You could remove this setup call if you don't like it, and try some other statusline plugin
       local statusline = require("mini.statusline")
       statusline.setup({ use_icons = vim.g.have_nerd_font })
