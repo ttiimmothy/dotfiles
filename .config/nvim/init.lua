95ae1fbc5822c2699f90d13d1a896dd2af6d5b7e
ttiimmothy <dinosauli2006@gmail.com>
2025-05-29 23:36:44 -0400

    feat remove unused comments in neovim configuration init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -307,7 +307,6 @@ require("lazy").setup({
   { import = "various.plugins" },
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = "⌘",
       config = "🛠",
