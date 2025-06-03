34e619205c4b7ce271e3ad8e2431c4b0c31371f2
ttiimmothy <dinosauli2006@gmail.com>
2025-06-02 22:00:45 -0400

    feat update fish alias

--- .config/fish/config-alias.fish
+++ .config/fish/config-alias.fish
@@ -12,7 +12,7 @@ function git
   end
 end
 # initial commit
-alias init "git a . && git co -m 'first init' && git push -u origin main"
+alias init "git a && git co -m 'first init' && git push -u origin main"
 # usually use after emt
 alias emback "git cout main && git pull-allow origin main"
 # checkout empty branch and create empty commit
