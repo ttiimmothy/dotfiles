fe7195897f009e0fa3a7d6f9e39b14df25d73601
Timothy <dinosauli2006@gmail.com>
2025-08-16 12:34:27 -0400

    config(fish) add alias

--- .config/fish/config-alias.fish
+++ .config/fish/config-alias.fish
@@ -15,6 +15,7 @@ alias emback "git cout main && git pull-allow origin main"
 alias emt "git cout empty && git pull origin empty && git pull origin main && git co -m "empty" && git push"
 alias git-global "git config --global --list"
 alias glk "git lfs track"
+alias gostow "z stow && emback && z stow"
 
 # neofetch
 alias icondistro "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
