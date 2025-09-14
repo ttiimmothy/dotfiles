9816c2ba7864ab8b96e29127e781f871bc47c908
Timothy <dinosauli2006@gmail.com>
2025-09-14 16:51:38 -0400

    config(fish) update alias

--- .config/fish/config-alias.fish
+++ .config/fish/config-alias.fish
@@ -37,18 +37,11 @@ alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
 alias raspberry "neofetch --ascii_colors 8 2 --ascii_distro raspbian --config none"
 
 # other (tmux, neovim, zoxide and more)
-alias vi "nvim"
-alias ni "nvim"
 alias vim "nvim"
 alias d "clear"
 alias python "python3"
 alias pip "pip3"
-alias wificonnectivity "ping -c 8 firebase.com"
-alias wificonnectivitycheck "ping -c 17 firebase.com"
-alias wificheck "ping -c 23 firebase.com"
-alias firebases "ping -c 24 firebase.com"
-alias ero "ping -c 20 firebase.com"
-alias timoptimothywificonnectivitycheck "ping -c 10 firebase.com"
+alias erc "ping -c 20 firebase.com"
 alias zc "zoxide query -l -s"
 
 # source
