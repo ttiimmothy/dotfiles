58efa07608e96b8ad3dcf383c6f51176584cf43c
Timothy <timothytimothytimo6@gmail.com>
2026-02-23 05:36:17 -0500

    config(fish):update fish alias

--- .config/fish/config-alias.fish
+++ .config/fish/config-alias.fish
@@ -84,9 +84,9 @@ alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
 alias raspberry "neofetch --ascii_colors 8 2 --ascii_distro raspbian --config none"
 
 # other (tmux, neovim, zoxide and more)
-alias tmu "tmux"
+alias tu "tmux"
 alias vim "nvim"
 alias d "clear"
-alias erc "ping -c 20 firebase.com"
-alias zc "zoxide query -l -s"
+alias ercfirese "ping -c 20 firebase.com"
+alias zls "zoxide query -l -s"
 alias srf "source ~/.config/fish/config.fish"
