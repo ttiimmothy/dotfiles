226e7dfe4945d34ea83f4120334a3ee1dfc6eb93
Timothy <timothytimothytimo6@gmail.com>
2026-02-23 00:17:58 -0500

    config(nvim):update config,delete lspconfiguration.lua,shortlisted.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -217,6 +217,124 @@ require("lazy").setup({
       end, { desc = "[S]earch [N]eovim files" })
     end,
   },
+  { -- Main LSP Configuration
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      'mason-org/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      -- Useful status updates for LSP.
+      { 'j-hui/fidget.nvim', opts = {} },
+      -- Allows extra capabilities provided by blink.cmp
+      'saghen/blink.cmp',
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+        callback = function(event)
+          -- NOTE: Remember that Lua is a real programming language, and as such it is possible to define small helper and utility functions so you don't have to repeat yourself
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+          -- Jump to the definition of the word under your cursor
+          -- To jump back, press <C-t>.
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          -- Opens a popup that displays documentation about the word under your cursor
+          -- See `:help K` for why this keymap.
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          -- Neovim 0.11 is now stable
+          -- ---@param client vim.lsp.Client
+          -- ---@param method vim.lsp.protocol.Method
+          -- ---@param bufnr? integer some lsp support methods only in specific files
+          -- ---@return boolean
+          -- local function client_supports_method(client, method, bufnr)
+          --   if vim.fn.has 'nvim-0.11' == 1 then
+          --     return client:supports_method(method, bufnr)
+          --   else
+          --     return client.supports_method(method, { bufnr = bufnr })
+          --   end
+          -- end
+          -- See `:help CursorHold` for information about when this is executed
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.clear_references,
+            })
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+          if client and client:supports_method('textDocument/inlayHint', event.buf) then
+            map('<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+          end
+        end,
+      })
+      -- tbl = table
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local servers = {
+        clangd = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
+        cssls = {},
+        tailwindcss = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+        },
+        html = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = "Replace",
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+      }
+      -- To check the current status of installed tools and/or manually install other tools, you can run
+      -- NOTE :Mason
+      require("mason").setup()
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        "stylua", -- Used to format Lua code
+      })
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+      for server_name, config in pairs(servers) do
+        vim.lsp.config(server_name, config)
+      end
+    end,
+  },
   { -- NOTE: replace tsserver with typescript-tools.nvim
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
@@ -334,7 +452,7 @@ require("lazy").setup({
 			TS.setup(opts)
     end,
   },
-  { import = "various.plugins" },
+  -- { import = "various.plugins" },
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
