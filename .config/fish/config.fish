95d827302356199b893481f5aee64427e1c9eb20
Timothy <timothytimothytimo6@gmail.com>
2026-02-24 10:53:37 -0500

    config(fish):update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,8 @@ if status is-interactive
     source (dirname (status --current-filename))/.profile
   end
   set -g fish_greeting
+  set -gx HOMEBREW_NO_ENV_HINTS 1
 end
 eval "$(/opt/homebrew/bin/brew shellenv)"
-set -gx HOMEBREW_NO_ENV_HINTS 1
 zoxide init fish | source
 pyenv init - fish | source
