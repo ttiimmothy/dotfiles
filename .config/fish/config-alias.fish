504b031578da053f8d178e2b366b3d5da11a1e58
Timothy <timothytimothytimo6@gmail.com>
2026-02-24 17:37:13 -0500

    config(fish):update alias,config(git):update .gitconfig

--- .config/fish/config-alias.fish
+++ .config/fish/config-alias.fish
@@ -5,22 +5,6 @@ Co-authored-by: antfu <github@antfu.me>
 Co-authored-by: hoipangcheung <hoipang1e06@gmail.com>
 Co-authored-by: yyx990803 <yyx990803@gmail.com>"
 
-function countbinary
-  # in \( -type f -or -type l \), f = file, l = symlink
-  # -perm +111 = executable
-  for dir in $PATH
-    if test -d $dir
-      find $dir -maxdepth 1 \( -type f -or -type l \) -perm +111 2>/dev/null | xargs -n1 basename
-    end
-  end | sort | uniq | wc -l | string trim
-end
-function listallbinaries
-  for dir in $PATH
-    if test -d $dir
-      find $dir -maxdepth 1 \( -type f -or -type l \) -perm +111 2>/dev/null | xargs -n1 basename
-    end
-  end | sort | uniq | vi
-end
 function git
   if test (count $argv) -eq 2; and test "$argv[1]" = "checkout"
     set -l branch_name $argv[2]
@@ -38,41 +22,34 @@ function git
   end
 end
 function nrot
-  gt co -m "$argv[1]"
-  gt push
+  git ci -m "$argv[1]"
+  git push
 end
 function nrotwithc
-  gt co -m "$argv[1]" -m "$ALL_AUTHORS
+  git ci -m "$argv[1]" -m "$ALL_AUTHORS
 Co-authored-by: ttiimmothy <timothytimothytimo6@gmail.com>"
-  gt push
+  git push
 end
 function nrotwithc-c
-  gt co -m "$argv[1]" -m "$ALL_AUTHORS
+  git ci -m "$argv[1]" -m "$ALL_AUTHORS
 Co-authored-by: ttiimmothhy <timothytimothytimo0@gmail.com>"
-  gt push
+  git push
 end
 function nrotwithc-w1
-  gt co -m "$argv[1]" -m "$ALL_AUTHORS
+  git ci -m "$argv[1]" -m "$ALL_AUTHORS
 Co-authored-by: ttiimmothy <timothytimothytimo3@gmail.com>"
-  gt push
+  git push
 end
 function nrotwithc-w2
-  gt co -m "$argv[1]" -m "$ALL_AUTHORS
+  git ci -m "$argv[1]" -m "$ALL_AUTHORS
 Co-authored-by: ttiimmothy <timothytimothytimo5@gmail.com>"
-  gt push
-end
-function initbranch
-  gt co -m "branch first init"
-  gt push
+  git push
 end
 function emback
-  set -l default_branch (gt remote show origin | sed -n '/HEAD branch/s/.*: //p')
-  gt checkout $default_branch && gt p origin $default_branch
+  set -l default_branch (git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+  git checkout $default_branch && git p origin $default_branch
 end
-alias init "gt . && nrot 'first init'"
-alias tstw "z stow && emback && z dot"
-alias gi "git"
-alias gt "git"
+alias init "git . && nrot 'first init'"
 
 # neofetch
 alias icondistro "neofetch --ascii_colors 9 8 --ascii_distro debian --config none --colors 3 2 7 6 2 8"
@@ -80,12 +57,6 @@ alias icondistrocolor "neofetch --ascii_colors 7 9 --ascii_distro debian --confi
 alias icon "neofetch --ascii_colors 9 8 --ascii_distro debian --config none -L"
 alias archdistro "neofetch --ascii_colors 8 9 --ascii_distro arch --config none --colors 2 7 7 3 7 7"
 alias arch "neofetch --ascii_colors 8 9 --ascii_distro arch --config none -L"
-alias tu "tmux"
 alias vim "nvim"
 alias d "clear"
 alias cat "bat"
-alias bew "brew"
-alias ercfirese "ping -c 20 firebase.com"
-alias zls "zoxide query -l -s | head -n 20"
-alias zcs "zoxide query -l -s | head -n 20"
-alias srf "source ~/.config/fish/config.fish"
