f4e1f6126c25ae48713735094494071fd6da3cfd
Timothy <timothytimothytimo6@gmail.com>
2026-02-28 11:43:48 -0500

    config(fish):update config,(karabiner):update keybinding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,8 @@ if status is-interactive
   source (dirname (status --current-filename))/config-alias.fish
   set -gx HOMEBREW_NO_ENV_HINTS 1
   set -g fish_greeting
-  if type -q eza
-    alias lla "eza -alg --icons"
-  end
+  alias lla "eza -alg --icons"
 end
 eval "$(/opt/homebrew/bin/brew shellenv)"
-zoxide init fish | source
 pyenv init - fish | source
+zoxide init fish | source
